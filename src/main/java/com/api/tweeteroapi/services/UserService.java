2141e00d7390c12267876d92d8611a9c1ac97398
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2023-01-23 17:57:13 -0300

    feat: create signup service

---
+++ src/main/java/com/api/tweeteroapi/services/UserService.java
@@ -0,0 +1,25 @@
+package com.api.tweeteroapi.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.tweeteroapi.dto.UserDTO;
+import com.api.tweeteroapi.model.UserModel;
+import com.api.tweeteroapi.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    @Autowired
+    private UserRepository repository;
+
+    public void save(UserDTO dto) {
+        repository.save(new UserModel(dto));
+    }
+
+    public List<UserModel> findall(){
+        return repository.findAll();
+    }
+}
