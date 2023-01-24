82e659c5f62baf48c1e3c11cedd0a18141de12e9
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2023-01-24 15:57:46 -0300

    feat: deleteting get users method for tests

--- src/main/java/com/api/tweeteroapi/controllers/UserController.java
+++ src/main/java/com/api/tweeteroapi/controllers/UserController.java
@@ -1,17 +1,13 @@
 package com.api.tweeteroapi.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.tweeteroapi.dto.UserDTO;
-import com.api.tweeteroapi.model.UserModel;
 import com.api.tweeteroapi.services.UserService;
 
 @RestController
@@ -22,12 +18,8 @@ public class UserController {
     private UserService service;
 
     @PostMapping
-    public void create(@RequestBody UserDTO req){
+    public String create(@RequestBody UserDTO req){
         service.save(req);
-    }
-
-    @GetMapping
-    public List<UserModel> listAll(){
-        return service.findall();
+        return "OK";
     }
 }
