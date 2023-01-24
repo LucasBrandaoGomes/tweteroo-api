a0a1f17245f0c297432a5c4b316044437bf1cc75
Lucas Brandão <88392678+LucasBrandaoGomes@users.noreply.github.com>
2023-01-24 14:30:38 -0300

    feat: add search by username

--- src/main/java/com/api/tweeteroapi/repository/UserRepository.java
+++ src/main/java/com/api/tweeteroapi/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.api.tweeteroapi.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.tweeteroapi.model.UserModel;
 
 public interface UserRepository extends JpaRepository<UserModel, Long>{
     
+    Optional<UserModel> findByUsername(String username);
 }
